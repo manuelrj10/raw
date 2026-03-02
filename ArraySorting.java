68105f3cec0b56b3e2a4e2149122344fb7636a3a
Manuel Rajesh <manuelrajesh39@gmail.com>
2026-03-02 10:56:53 +0530

    Add files via upload

---
+++ ArraySorting.java
@@ -0,0 +1,22 @@
+package pgm;
+
+import java.util.Arrays;
+
+public class ArraySorting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {5,4,3,2,2};
+		for(int i=0;i<arr.length-1;i++) {
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[i]>arr[j]) {
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}System.out.println(Arrays.toString(arr));
+
+	}
+
+}
